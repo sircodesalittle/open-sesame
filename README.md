1c6fb4ca971609447e747cc4a0279f5f946658d9
Alex Dykstra <adykstra1406@gmail.com>
2018-11-16 09:44:13 -0500

    First try at garage door trigger endpoint

--- README.md
+++ README.md
@@ -6,12 +6,3 @@ The backend is an express.js server that executes BACnet commands needed to do a
 
 1. Trigger garage door open/close
 2. Retrieve status as to whether the door is open or closed
-
-
-
-### TODO
-- BACnet command to initiate garage door movement
-- BACnet command to retrieve status of garage being open (This is a discrete status - if not closed, then it is open - it does not indicate how far the door is open)
-- Configuration for local BACnet addressing (the server running the web application)
-- Configuration for remote BACnet addressing (the device that interacts with the garage door)
-- HTTP endpoints for initiating garage door movement
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,59 @@
-var express = require('express');
-var router = express.Router();
+const bacnet = require('bacstack');
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const client = new bacnet();
+
+const GARAGE_IP = '172.31.218.40'
+const GARAGE_INSTANCE = 520401;
+const GARAGE_OPENER_BV_INSTANCE = 42
+
+function sleep(ms){
+  return new Promise(resolve=>{
+      setTimeout(resolve,ms)
+  })
+}
+
+class Garage {
+
+  constructor(ip, deviceInstance, bvInstance) {
+    this.ip = ip;
+    this.deviceInstance = deviceInstance;
+    this.bvInstance = bvInstance;
+    this.client = new bacnet();
+  }
+
+  writeBvPresentValue(value) {
+    this.client.writeProperty(this.ip, {type: 5, instance: this.bvInstance}, 85, [
+      {type: bacnet.enum.ApplicationTags.BACNET_APPLICATION_TAG_ENUMERATED, value: value}
+    ], (err, value) => {
+      console.log('value: ', value);
+    });
+  }
+
+  startTriggerDoor() {
+    this.writeBvPresentValue(0);
+  }
+
+  stopTriggerDoor() {
+    this.writeBvPresentValue(1);
+  }
+
+  async triggerDoor() {
+    this.startTriggerDoor();
+    await sleep(2000);
+    this.stopTriggerDoor();
+  }
+
+}
+
+
+const garage = new Garage(GARAGE_IP, GARAGE_INSTANCE, GARAGE_OPENER_BV_INSTANCE);
+
+router.get('/trigger-door', function(req, res, next) {
+  garage.triggerDoor();
+  res.json({'status': 'success'}) // TODO what do we want to return? - prolly want to make the triggerDoor return a promise
 });
+
 
 module.exports = router;
